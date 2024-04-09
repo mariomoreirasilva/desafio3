ac00771d0400618bb03c63da20e2f2af3b05329b
mariomoreirasilva <mario@desban.org.br>
2024-04-09 20:35:02 -0300

    Teste repositorio

--- src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -1,16 +1,28 @@
 package com.devsuperior.desafio3.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.desafio3.entities.Client;
+import com.devsuperior.desafio3.repositories.ClientRepository;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 	
+	@Autowired
+	private ClientRepository repository;
+	
 	@GetMapping
 	public String teste() {
-		return "Ola mundo";
+		
+		Optional<Client> result = repository.findById(1L);
+		Client client = result.get();
+		return client.getName();
 		
 	}
 	
