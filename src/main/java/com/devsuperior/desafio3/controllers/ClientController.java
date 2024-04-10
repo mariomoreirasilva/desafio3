fb216b53ba96bdaac3836309d95a6a8b976f8313
mariomoreirasilva <mario@desban.org.br>
2024-04-10 10:35:15 -0300

    FindAll paginado

--- src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -1,16 +1,16 @@
 package com.devsuperior.desafio3.controllers;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.desafio3.dto.ClientDTO;
-import com.devsuperior.desafio3.entities.Client;
-import com.devsuperior.desafio3.repositories.ClientRepository;
 import com.devsuperior.desafio3.services.ClientService;
 
 @RestController
@@ -25,6 +25,11 @@ public class ClientController {
 		ClientDTO dto = service.findById(id);
 		return dto;
 	}
-	
+	//busca paginada
+	@GetMapping()
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<ClientDTO> result = service.findAll(pageable);
+		return result;
+	}
 	
 }
--- src/main/java/com/devsuperior/desafio3/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/services/ClientService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.desafio3.services;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,12 @@ public class ClientService {
 		Client client = repository.findById(id).get();		
 		return new ClientDTO(client);
 	}
-	
+
+	//Busca paginada
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);		
+		return result.map(x -> new ClientDTO(x));
+	}
 }
+
